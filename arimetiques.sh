08a1c91316e159457c3b701f4a41a2727add35ab
Francesc <fbusquetsf@esliceu.net>
2026-04-30 13:27:43 +0200

    primeres operacions arimètiques

---
+++ arimetiques.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#aquest script ens mostra com
+#realitzar les principals operacions arimètiques
+
+read -p "Introdueix el valor de X: " X
+read -p "Introdueix el valor de Y: " Y
+
+echo "Has introduït ${X} i ${Y}"
