a9be7e663bc3b5cab430d9946a2d2dfb1372b28c
Francesc <fbusquetsf@esliceu.net>
2026-05-03 15:50:08 +0200

    usage

--- user.sh
+++ user.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo "Hello world"
+function usage (){
+    echo "Usage ${0}"
+    echo
+    echo "Has de ser root"
+    exit 1
+}
+
+#Script per crear un usuari
+
+#primer comprovam si som l'usuari root
+if [[ ${UID} -ne 0 ]]
+then
+    usage
+fi
 
