d7acdf1b12d47d167b733fb205fa59ba089a0d6d
Francesc <fbusquetsf@esliceu.net>
2026-05-13 20:30:31 +0200

    a

---
+++ sumatorios.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[$# -ne 1]]
+then
+    echo "Error: se requiere un único parámetro"
+    exit 1
+fi
+echo "vamos a calcular el sumatorio de $1"
+#calcular el sumatorio de un numero
+exit 1
