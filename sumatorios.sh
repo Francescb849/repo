cb551193f52b3931f97dfa67bff5778a20abd5d1
Francesc <fbusquetsf@esliceu.net>
2026-05-14 11:41:50 +0200

    video sumatorios Guillem

--- sumatorios.sh
+++ sumatorios.sh
@@ -6,5 +6,25 @@ then
     exit 1
 fi
 echo "vamos a calcular el sumatorio de $1"
+
+#comprobar que realmente tenemos un número
+
+if ! [[ "$1" =~ ^[0-9]+$  ]]; then
+    echo "ERROR: SE REQUIERE UN NUMERO"
+    exit 1
+fi
+
+#recoger valor de n
+n=$1
+suma=0
+
+#bucle for
+
+for (( i=i;i<=n;i++ ))
+do
+    suma=$((suma+i))
+done
+
+echo "el sumatorio de $n vale $suma"
 #calcular el sumatorio de un numero
-exit 1
+exit 0
